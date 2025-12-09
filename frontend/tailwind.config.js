aaf60afa68049647468eb1fe44be1cb05ab69283
Akhilesh Desai <akhileshdesai19@gmail.com>
2025-12-09 13:32:44 +0530

    added navbar and header

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': "#5f6FFF",
+      },
+    },
   },
   plugins: [],
 };
+
