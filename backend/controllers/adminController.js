a57c56c07f5737f4edd718d77a52804e4bf3b35a
Akhilesh Desai <akhileshdesai19@gmail.com>
2025-12-21 11:12:39 +0530

    backend login

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,22 @@
+
+
+
+// API for adding doctor
+
+const addDoctor = async (req,res) => {
+
+    try {
+
+        const { name, email, password, speciality, degree, experience, about, fees, address} = req.body
+        const imageFile = req.file
+
+        // checkin for all data to add doctor
+        if() {
+
+        }
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
