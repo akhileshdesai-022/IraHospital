5f7e38aed4448b2f4d1fd33e5d4e1866129d3452
Akhilesh Desai <akhileshdesai19@gmail.com>
2025-12-29 17:17:51 +0530

    dashboard page for admin panel

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { useEffect } from 'react'
 
 const Dashboard = () => {
+
+  const {aToken , getDashData, cancelAppointment, dashDate } = useContext(AdminContext)
+
+   useEffect(()=>{
+    if (aToken) {
+      getDashData()
+      
+    }
+
+   },[aToken])
+
+
   return (
-    <div>Dashboard</div>
+    <div>
+
+    </div>
   )
 }
 
